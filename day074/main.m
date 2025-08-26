1d525851ec2a796f8d5eb02b63c3ece26d01525b
El <oloyedeelijah23@gmail.com>
2025-08-26 19:48:30 +0100

    Add README, main.m, and circuit files for Day 074: Random Points Clustering and Plot

---
+++ day074/main.m
@@ -0,0 +1,8 @@
+
+% Day 074: Random Points Clustering and Plot
+
+X = [randn(50,2)+2; randn(50,2)-2];
+idx = kmeans(X,2);
+gscatter(X(:,1), X(:,2), idx);
+title('Random Points Clustering');
+xlabel('X'); ylabel('Y');
