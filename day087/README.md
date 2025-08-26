bcd785955dd684061674ee9c737c0e0a71fc6a9e
El <oloyedeelijah23@gmail.com>
2025-08-26 19:50:40 +0100

    Add README, main.m, and circuit files for Day 087: Random Scatter Plot

---
+++ day087/README.md
@@ -0,0 +1,3 @@
+# Day 087: Random Scatter Plot
+
+Generates random x and y data and visualizes them using a scatter plot in MATLAB.
---
+++ day087/main.m
@@ -0,0 +1,8 @@
+% Day 087: Random Scatter Plot
+x = randn(1, 100);
+y = randn(1, 100);
+scatter(x, y);
+title('Random Scatter Plot');
+xlabel('X');
+ylabel('Y');
+grid on;
