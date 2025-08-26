bd7479df5dda0d6ad19d0fddf18d809e8b0f24bb
El <oloyedeelijah23@gmail.com>
2025-08-26 19:52:06 +0100

    Add README and main.m files for Day 100: Random Points Clustering and Plot

---
+++ day100/README.md
@@ -0,0 +1,3 @@
+# Day 100: Random Points Clustering and Plot
+
+Generates random points, clusters them using k-means, and visualizes the clusters in MATLAB.
---
+++ day100/main.m
@@ -0,0 +1,7 @@
+% Day 100: Random Points Clustering and Plot
+X = [randn(50,2)+2; randn(50,2)-2];
+idx = kmeans(X,2);
+gscatter(X(:,1), X(:,2), idx);
+title('Random Points Clustering');
+xlabel('X');
+ylabel('Y');
