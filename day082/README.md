a1d124cfdbdb7868c4e948ead45bb877e785a1d8
El <oloyedeelijah23@gmail.com>
2025-08-26 19:50:07 +0100

    Add README, main.m, and circuit files for Day 082: Random RGB Image

---
+++ day082/README.md
@@ -0,0 +1,3 @@
+# Day 082: Random RGB Image
+
+Generates and displays a random RGB image using MATLAB.
---
+++ day082/main.m
@@ -0,0 +1,4 @@
+% Day 082: Random RGB Image
+img = randi([0, 255], 100, 100, 3, 'uint8');
+imshow(img);
+title('Random RGB Image');
