ab27c29367016ba401e4474f5f901a5361d2c7ac
El <oloyedeelijah23@gmail.com>
2025-08-26 19:50:53 +0100

    Add README and main.m files for Day 089: Random Surface Plot

---
+++ day089/README.md
@@ -0,0 +1,3 @@
+# Day 089: Random Surface Plot
+
+Generates a random surface and visualizes it using a 3D surface plot in MATLAB.
---
+++ day089/main.m
@@ -0,0 +1,8 @@
+% Day 089: Random Surface Plot
+[X, Y] = meshgrid(1:0.5:10, 1:0.5:10);
+Z = rand(size(X)) * 10;
+surf(X, Y, Z);
+title('Random Surface Plot');
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
