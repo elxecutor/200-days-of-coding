f235cbc31dfe31886bb19ef51162ca60f97f7371
El <oloyedeelijah23@gmail.com>
2025-08-26 19:51:23 +0100

    Add README and main.m files for Day 094: Random Prime Finder

---
+++ day094/README.md
@@ -0,0 +1,3 @@
+# Day 094: Random Prime Finder
+
+Generates a random array and finds all prime numbers within it using MATLAB.
---
+++ day094/main.m
@@ -0,0 +1,5 @@
+% Day 094: Random Prime Finder
+A = randi([1, 100], 1, 20);
+primes_in_A = A(isprime(A));
+disp('Random Array:'); disp(A);
+disp('Prime Numbers:'); disp(primes_in_A);
