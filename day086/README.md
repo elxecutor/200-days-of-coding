8c0e0620245e297da40cd4a7c509bd863067729f
El <oloyedeelijah23@gmail.com>
2025-08-26 19:50:33 +0100

    Add README, main.m, and circuit files for Day 086: Random Bar Chart

---
+++ day086/README.md
@@ -0,0 +1,3 @@
+# Day 086: Random Bar Chart
+
+Generates random data and visualizes it using a bar chart in MATLAB.
---
+++ day086/main.m
@@ -0,0 +1,6 @@
+% Day 086: Random Bar Chart
+A = randi(100, 1, 10);
+bar(A);
+title('Random Bar Chart');
+xlabel('Index');
+ylabel('Value');
