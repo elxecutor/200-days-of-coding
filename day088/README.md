a2e5d8c7c920e3f2d5b124d894b467db44837247
El <oloyedeelijah23@gmail.com>
2025-08-26 19:50:46 +0100

    Add README, main.m, and circuit files for Day 088: Depletion-Mode PMOS Transistor Circuit

---
+++ day088/README.md
@@ -0,0 +1,3 @@
+# Day 088: Random Histogram
+
+Generates random data and visualizes it using a histogram in MATLAB.
---
+++ day088/main.m
@@ -0,0 +1,6 @@
+% Day 088: Random Histogram
+X = randi(100, 1, 200);
+histogram(X, 20);
+title('Histogram of Random Integers');
+xlabel('Value');
+ylabel('Frequency');
